3573d5ff67edd57680ee624ad230d3737108764b
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-08-20 22:54:45 +0700

    feat: using enum for category prop

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:uuid/uuid.dart';
 
 Uuid _uuid = const Uuid();
@@ -7,10 +8,12 @@ class Expense {
     required this.title,
     required this.amount,
     required this.date,
+    required this.category,
   }) : id = _uuid.v4();
 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 }
