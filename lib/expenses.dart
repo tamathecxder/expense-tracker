615dc89281f1243152c7422d5392b7aa57c0bf9d
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-08-25 21:27:29 +0700

    feat(expenses): add delete message on snackbar and display default content if the data is empty

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -50,13 +50,45 @@ class _ExpensesState extends State<Expenses> {
   }
 
   void _removeExpense(Expense expense) {
+    var index = _registeredExpenses.indexOf(expense);
+
     setState(() {
       _registeredExpenses.remove(expense);
     });
+
+    ScaffoldMessenger.of(context).clearSnackBars();
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('Expense deleted.'),
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(index, expense);
+            });
+          },
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget displayedContent = const Center(
+      child: Text(
+        'No expenses found yet. Start adding some!',
+      ),
+    );
+
+    if (_registeredExpenses.isNotEmpty) {
+      displayedContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Expense Tracker"),
@@ -70,10 +102,7 @@ class _ExpensesState extends State<Expenses> {
       body: Column(
         children: [
           Expanded(
-            child: ExpensesList(
-              expenses: _registeredExpenses,
-              onRemoveExpense: _removeExpense,
-            ),
+            child: displayedContent,
           ),
         ],
       ),
