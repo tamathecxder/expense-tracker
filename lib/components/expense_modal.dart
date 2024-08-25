fe570ec084aeb64a2c8a6d8f1eee0156ab1cc6b6
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-08-25 10:00:44 +0700

    chore(expense_modal): show date picker

--- lib/components/expense_modal.dart
+++ lib/components/expense_modal.dart
@@ -19,6 +19,13 @@ class _ExpenseModalState extends State<ExpenseModal> {
     _amountController.dispose();
   }
 
+  void _openDatePicker() {
+    var firstDate = DateTime(DateTime.now().year - 1);
+    var lastDate = DateTime.now();
+
+    showDatePicker(context: context, firstDate: firstDate, lastDate: lastDate);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -31,25 +38,61 @@ class _ExpenseModalState extends State<ExpenseModal> {
             maxLength: 50,
             decoration: const InputDecoration(labelText: 'Title'),
           ),
-          TextField(
-            keyboardType: TextInputType.number,
-            controller: _amountController,
-            decoration: const InputDecoration(labelText: 'Amount'),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  controller: _amountController,
+                  decoration: const InputDecoration(
+                    prefixText: '\$ ',
+                    labelText: 'Amount',
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text("Selected Month"),
+                    IconButton(
+                        onPressed: _openDatePicker,
+                        icon: Icon(Icons.calendar_month))
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 20,
           ),
           Row(
             children: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text(
+                  'Cancel',
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
               ElevatedButton(
                 onPressed: () {
                   print(_titleController.text);
                 },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue[400],
+                  foregroundColor: Colors.white,
+                ),
                 child: const Text(
                   'Save Expense',
-                ),
-              ),
-              TextButton(
-                onPressed: () {},
-                child: Text(
-                  'Cancel',
                 ),
               ),
             ],
